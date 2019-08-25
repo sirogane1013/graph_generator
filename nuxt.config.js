966f843118fcc315aa9731fefe14d2be7c859e0e
siroane1013 <stlouis.route66@gmail.com>
2019-08-25 17:34:06 +0900

    configuration for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,9 @@
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/graph_generator/'
+  }
+} : {};
 
 export default {
   mode: 'spa',
@@ -48,5 +54,6 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
+  ...routerBase,
 }
